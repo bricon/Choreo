e9595a06a5c80a2bab4ab206dc2f397157863ac7
briyonce <admin@dRuNkInLuv.local>
2014-03-29 19:54:19 -0500

    made videos data

--- Choreo/videos/models.py
+++ Choreo/videos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+	title = models.CharField(max_length=200)
+	user = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	style = models.CharField(max_length=200)
+	choreographer = models.CharField(max_length=200)
+	song_title = models.CharField(max_length=200)
+	song_artist = models.CharField(max_length=200)
