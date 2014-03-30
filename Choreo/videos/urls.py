3102f13a6de09346a127d0aea0637c5c81c1dcc4
briyonce <admin@dRuNkInLuv.local>
2014-03-29 21:04:06 -0500

    added the new files

---
+++ Choreo/videos/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from videos import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<video_id>\d+)/$', views.detail, name='detail')
+)
