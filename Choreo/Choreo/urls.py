4b2edfc15da69700eaac14ab555a90dc9fa89801
briyonce <admin@dRuNkInLuv.local>
2014-04-03 18:37:56 -0500

    added CSS stuff

--- Choreo/Choreo/urls.py
+++ Choreo/Choreo/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^videos/', include('videos.urls')),
+    url(r'^videos/', include('videos.urls', namespace = "videos")),
     url(r'^admin/', include(admin.site.urls)),
 )
